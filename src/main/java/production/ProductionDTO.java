c76637ac3e62630534955ab0ab5e8c141c906f2e
Andrei-Laurentiu Radu <andreilaurentiuradu@gmail.com>
2023-12-25 18:57:08 +0200

    second commit i wish

--- src/main/java/production/ProductionDTO.java
+++ src/main/java/production/ProductionDTO.java
@@ -8,12 +8,12 @@ import java.util.List;
 public class ProductionDTO {
     public String title;
     public String type;
-    public List<String> actors;
-    public List<String> directors;
-    public List<Genre> genres;
-    public List<Rating> ratings;
-    public String subject;
-    public Double grade;
+//    public List<String> actors;
+//    public List<String> directors;
+//    public List<Genre> genres;
+//    public List<Rating> ratings;
+//    public String subject;
+//    public Double grade;
 
     public Production toProduction() {
         Production production = new Production() {
@@ -23,11 +23,12 @@ public class ProductionDTO {
             }
         };
 
-        production.setGrade(grade);
+//        production.setGrade(grade);
         production.setType(type);
         production.setTitle(title);
 
         return production;
     }
-
 }
+
+
